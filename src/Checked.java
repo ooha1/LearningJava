42e43218069971dc2306d0ef4d597400c97256c6
vad <oohavadlamudi.31@gmail.com>
2021-11-09 14:46:35 -0600

    commented

--- src/Checked.java
+++ src/Checked.java
@@ -2,7 +2,7 @@ public class Checked {
     public static void main(String[] args) {
         int a[] = {10, 20, 30, 40, 50};
         System.out.println(a[0]);
-        System.out.println(a[1]);
+        //System.out.println(a[1]);
 
     }
 }
